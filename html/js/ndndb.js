85e076e4554e5c5e7ad82abaf829ee833f87a213
wentaoshang <wentaoshang@gmail.com>
2014-12-10 21:18:24 -0800

    rename melnitz data points and import all names into db

--- html/js/ndndb.js
+++ html/js/ndndb.js
@@ -1,8 +1,3 @@
-var DataStat = function DataStat (prfx) {
-  this.prefix = prfx; // prefix for the data namespace
-  this.sample_num = 0; // total number of collected data
-};
-
 var onData = function (inst, data) {
   fetchDecryptionKey(data);
 };
@@ -43,31 +38,46 @@ var processData = function (data, sym_key) {
   var json_text = p1.toString(CryptoJS.enc.Utf8) + p2.toString(CryptoJS.enc.Utf8);
   var json_obj = jQuery.parseJSON(json_text);
 
-  dataStat.sample_num++;
+  data_num++;
 
-  if (dataStat.sample_num >= max_data_num) {
-    $("#result").append("Imported " + dataStat.sample_num + " data points.")
+  if (data_num >= 310) {
+    $("#result").append("Imported " + data_num + " data points.")
       .append("<br/>");
     $("#query").val("SELECT * FROM bms;");
     $("#run").attr("disabled", false);
   } else {
     // Insert data into db
     var stmt = db.prepare("INSERT INTO bms VALUES (?, ?, ?, ?, ?, ?);");
-    stmt.bind(["melnitz", "studio1", "total", "demand", json_obj.ts, json_obj.val]);
+    stmt.bind([Name.toEscapedString(data_name.components[3]),
+               Name.toEscapedString(data_name.components[5]),
+               Name.toEscapedString(data_name.components[7]),
+               Name.toEscapedString(data_name.components[8]),
+               json_obj.ts, json_obj.val]);
     stmt.step();
     stmt.free();
 
-    // Send interest for the next content object
-    var tpos = data_name.components.length - 1;
-    var ts = data_name.components[tpos];
-    var filter = new Exclude([ts, Exclude.ANY]);
-    var template = new Interest();
-    template.childSelector = 1;
-    template.interestLifetime = 4000;
-    template.exclude = filter;
-    template.setMustBeFresh(true);
+    if (data_num % 10 == 0) {
+      data_index++;
+      data_prefix = new Name(data_points[data_index].name);
+      var template = new Interest();
+      template.childSelector = 1;
+      template.setMustBeFresh(true);
+      template.interestLifetime = 4000;
 
-    face.expressInterest(dataStat.prefix, template, onData, onTimeout);
+      face.expressInterest(data_prefix, template, onData, onTimeout);
+    } else {
+      // Send interest for the next content object
+      var tpos = data_name.components.length - 1;
+      var ts = data_name.components[tpos];
+      var filter = new Exclude([ts, Exclude.ANY]);
+      var template = new Interest();
+      template.childSelector = 1;
+      template.interestLifetime = 4000;
+      template.exclude = filter;
+      template.setMustBeFresh(true);
+
+      face.expressInterest(data_prefix, template, onData, onTimeout);
+    }
   }
 };
 
@@ -78,20 +88,21 @@ var onTimeout = function (inst) {
   $("#error").fadeIn(100);
 };
 
-var dataStat;
+var data_num = 0; // total number of collected data
+var data_index = 0;
+var data_prefix;
+var max_data_num = 100;
 
 function loadData() {
   $("#result").append("Loading data...").append("<br/>");
 
-  var name = new Name(data_points[data_index].name);
-  dataStat = new DataStat(name);
-
+  data_prefix = new Name(data_points[data_index].name);
   var template = new Interest();
   template.childSelector = 1;
   template.setMustBeFresh(true);
   template.interestLifetime = 4000;
-  
-  face.expressInterest(name, template, onData, onTimeout);
+
+  face.expressInterest(data_prefix, template, onData, onTimeout);
 }
 
 function executeQuery () {
@@ -133,8 +144,7 @@ function executeQuery () {
 var db;
 var face;
 var hub = "borges.metwi.ucla.edu";
-var data_index = 0;
-var max_data_num = 200;
+
 var schema = ["BUILDING", "ROOM", "PANEL", "TYPE", "TIMESTAMP", "VALUE"];
 
 $(document).ready(function () {
